8e38af0e405e493cbc7323a3e42ed0a657841568
officialsaurav <officialsauravg@gmail.com>
2026-02-07 22:47:01 +0545

    operators

--- python/first program.py
+++ python/first program.py
@@ -1,4 +1,11 @@
+# basic date types in python 
 1+3
 4-8
 print("Hello, World!")
+print(type("Hello, World!") )
+age=19
+dec=10.9
+print(type(age))    
+print(type(dec))    
+print(type(age/age))
 #added new file in this nsame folder and added things
